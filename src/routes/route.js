a462ed776cc2d3c70d26bc75a1ca3f49b50b8ad2
Prashannjeet <prashannjeet02@gmail.com>
2022-02-23 01:14:54 +0530

    k

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+router.get("/test-me", function (req, res) {
+  res.send("My first ever api!");
+});
+
+router.get("/movies", function (req, res) {
+  res.send(["hero", "coolie", "khan", "titanic"]);
+});
+
+router.get("/students/:id", function (req, res) {
+  let x = req.params.id;
+  res.send("Rahul");
+});
+
+router.get("/movies/:movieId", function (req, res) {
+  let xe = ["hero", "titanic", "khan"];
+  let value = req.params.movieId;
+  if (value > xe.length - 1) {
+    res.send("dosent exit");
+  } else {
+    res.send(xe[value]);
+  }
 });
 
 module.exports = router;
